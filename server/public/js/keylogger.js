c7ee2d229c2a8428c5146af4f5fdf1c83ee1d0b0
Quema <myoon0929@gmail.com>
2023-09-28 00:37:56 +0900

    fin.

--- server/public/js/keylogger.js
+++ server/public/js/keylogger.js
@@ -4,12 +4,19 @@ let socket;
 window.onload = () => {
     socket = new WebSocket('ws://localhost:8768');
     const logList = document.getElementById('log-list');
+    const stream = document.getElementById('Stream')
+    stream.innerHTML  = 'WebSocket connection is pending'
+    logList.style.display = 'none'
 
     socket.onopen = (event) => {
         console.log('WebSocket connection opened.');
+        stream.innerHTML  = 'Keylogger pending<span class="circle" id="circle1"></span><span class="circle" id="circle2"></span><span class="circle" id="circle3"></span>'
     };
 
     socket.onmessage = (event) => {
+        logList.style.display = null
+        stream.innerHTML = null
+        stream.style.display="none"
         const logItem = document.createElement('li');
         logItem.textContent = event.data;
         logList.appendChild(logItem);
@@ -19,8 +26,14 @@ window.onload = () => {
     socket.onclose = (event) => {
         if (event.wasClean) {
             console.log(`WebSocket connection closed cleanly, code=${event.code}, reason=${event.reason}`);
+            stream.style.display= null
+            logList.style.display = 'none'
+            stream.textContent  = 'WebSocket connection closed cleanly'
         } else {
             console.error('WebSocket connection died');
+            stream.style.display= null
+            logList.style.display = 'none'
+            stream.textContent  = 'WebSocket connection died'
         }
     };
 
